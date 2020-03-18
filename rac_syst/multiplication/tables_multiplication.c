6b202a027d5a2c1bb636c205d526a1747bb63f69
Sofia MC <lsofmascastanio@gmail.com>
2020-03-18 16:23:57 +0100

    dossier multiplication

---
+++ rac_syst/multiplication/tables_multiplication.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+
+
+
+void traitementDesTables(int m1, int m2)
+{
+  m1= rand();
+  m2= rand();
+  printf("Quelle est la valeur de %d fois %d? ", m1, m2);
+}
+
+int main(){
+  int ponctuation;
+  pontuation = 0;
+
+
+}
