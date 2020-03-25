8bc1d71f16219e6dedf92eb561754b4f3a8090a3
Sofia MC <lsofmascastanio@gmail.com>
2020-03-25 10:47:02 +0100

    exo 1.2 js

--- js/exo1.js
+++ js/exo1.js
@@ -15,3 +15,16 @@ console.log("Minimum entre 2 et 8 : " + min(2, 8));
 console.log("Minimum entre 8 et 2 : " + min(8, 2));
 console.log("Maximum entre 2 et 8 : " + max(2, 8));
 console.log("Maximum entre 8 et 2 : " + min(8, 2));
+
+function numbers(i){
+  for(i=1; i<=100; i++){
+    if(i%3==0){
+      console.log("woueeeee" + i);
+    }else if(i%5==0){
+      console.log("yoooo" + i);
+    }else if(i == 73){
+      console.log("bingooo");
+    }
+    console.log(i);
+  }
+}
