98819b3f3b5852c7f075a2657b95a7bdf4cfe7c1
Sofia MC <lsofmascastanio@gmail.com>
2020-03-27 11:54:08 +0100

    exo2.2

--- js/exo2.js
+++ js/exo2.js
@@ -1,5 +1,5 @@
 'use strict';
-//création d'une fonction multiplicateur
+//1.création d'une fonction multiplicateur
 //qui reçoit un parametre entier n et retourne
 //une fonction qui multiplie son parametre x par n.
 
@@ -11,3 +11,20 @@ function creerMultplicateur(n){
 
 let multip= creerMultplicateur(2);
 multip(5);
+console.log(multip(5));
+
+//2. fonction creerSequence() qui resoit comme param
+// valeur d'increment step et retourne une fonction
+// qui delivere à chaque appel les valeurs successives
+// de la séquence demarrane à init et incrementes
+// de step.
+function creerSequence(init, step){
+
+    return () => (init += step++);
+}
+
+let s= creerSequence(2, 1);
+
+
+console.log(s());
+console.log("on ajoute" + s());
